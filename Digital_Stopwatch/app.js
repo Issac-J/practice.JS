49193be9c9b9dd2e1d1b91dc1123ef60de2393df
EggsyJ <58285674+JeonChanMin@users.noreply.github.com>
2023-04-27 18:02:44 +0900

    Fixed some Bugs

--- Digital_Stopwatch/app.js
+++ Digital_Stopwatch/app.js
@@ -2,6 +2,7 @@
 const containerEl = document.querySelector(".container");
 const secondsEl = document.querySelector("#seconds");
 const miliSecondsEl = document.querySelector("#mili-seconds");
+const recodeListEl = document.querySelector("#recodeList");
 
 // const buttonEls = document.querySelectorAll("button");
 const startButton = document.getElementById("startButton");
@@ -10,6 +11,7 @@ const recodeButton = document.getElementById("recodeButton");
 const resetButton = document.getElementById("resetButton");
 
 // console.log(startButton, stopButton, recodeButton, resetButton);
+console.log(recodeListEl);
 
 // setInterval(() => {
 //   console.log(123);
@@ -21,20 +23,22 @@ let miliSeconds = 0;
 
 // startButton 누르면 timer 시작
 startButton.addEventListener("click", () => {
+  clearInterval(interval);
+
   interval = setInterval(() => {
-    if (seconds < 10) {
-      secondsEl.innerText = "0" + seconds;
-    } else {
-      secondsEl.innerText = seconds;
-    }
+    // seconds 자리가 10보다 작은 경우, 두자리로 표시하기 위함
+    if (seconds < 10) secondsEl.innerText = "0" + seconds;
+    else secondsEl.innerText = seconds;
 
     miliSeconds++;
+
     if (miliSeconds > 99) {
       seconds++;
       miliSeconds = 0;
     }
 
-    miliSecondsEl.innerText = miliSeconds;
+    if (miliSeconds < 10) miliSecondsEl.innerText = "0" + miliSeconds;
+    else miliSecondsEl.innerText = miliSeconds;
   }, 10);
 });
 
@@ -48,4 +52,12 @@ resetButton.addEventListener("click", () => {
   miliSeconds = 0;
   secondsEl.innerText = "00";
   miliSecondsEl.innerText = "00";
+  recodeListEl.textContent = "";
+});
+
+recodeButton.addEventListener("click", () => {
+  const recodeDiv = document.createElement("div");
+  recodeListEl.appendChild(recodeDiv);
+  recodeDiv.id = Number(new Date());
+  recodeDiv.innerText = `${secondsEl.innerText}:${miliSecondsEl.innerText}`;
 });
